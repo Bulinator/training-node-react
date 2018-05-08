9d49394fda38d9d0622d9f818ca19120e60fbf55
Speudyland <speud@bubul.be>
2018-05-08 15:29:23 +0000

    add googleStrategy lib

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 // generate new app
 const app = express();
 
+/*
 app.get('/', (req, res) => {
   res.send({ bye: 'Jean-Claude' });
 });
+*/
+
+// refactoring this part later
+// http://console.developers.google.com
+passport.use(new GoogleStrategy());
 
 // run server
 const PORT = process.env.PORT || 5000;
