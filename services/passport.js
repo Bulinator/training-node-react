1f56dd9c3da9783fe6cb709846f8815eca1807c2
bulotge <bulotge@gisis.eu>
2018-05-16 15:38:03 +0200

    Add serialize and deserialize User to store cookie session

--- services/passport.js
+++ services/passport.js
@@ -5,12 +5,36 @@ const keys = require('../config/keys.js');
 
 const User = mongoose.model('users');
 
+// call when user is logged, we will get the profile id
+// saved to cookie session req.session.passport.user = {id:'..'}
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// user object attaches to the request as req.user
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   },
-  (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save();
+  async (accessToken, refreshToken, profile, done) => {
+    await mongoose.findOne({ 'googleId': profile.id })
+      .then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // We do not have a user record with this ID, make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
   })
 );
