1b86901165afd3814a0e5e7b3b4fc70cddb88425
bulotge <bulotge@gisis.eu>
2018-05-16 17:34:35 +0200

    review code buggy

--- index.js
+++ index.js
@@ -3,37 +3,23 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
-// load User model
 require('./models/User');
-// execute passport strategy
-require('passport');
+require('./services/passport');
 
-// connect mongoodb
 mongoose.connect(keys.mongoURI);
 
-// generate new app
 const app = express();
 
 app.use(
   cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in seconds
+    maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
 );
-
-// load cookie session in passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-// call authRoutes (js tricks)
-// require return a function, with an immediately invokes app
 require('./routes/authRoutes')(app);
 
-
-// refactoring this part later
-// http://console.developers.google.com
-// run server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Bulinator dev server is running on port ${PORT}`);
-});
+app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -1,40 +1,42 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
-const keys = require('../config/keys.js');
+const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
-// call when user is logged, we will get the profile id
-// saved to cookie session req.session.passport.user = {id:'..'}
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-// user object attaches to the request as req.user
 passport.deserializeUser((id, done) => {
   User.findById(id).then(user => {
     done(null, user);
   });
 });
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    await mongoose.findOne({ 'googleId': profile.id })
-      .then((existingUser) => {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).exec().then(existingUser => {
         if (existingUser) {
           // we already have a record with the given profile ID
           done(null, existingUser);
         } else {
-          // We do not have a user record with this ID, make a new record
+          // we don't have a user record with this ID, make a new record!
           new User({ googleId: profile.id })
             .save()
             .then(user => done(null, user));
         }
+      })
+      .catch(error => {
+        console.log('Error: ', error);
       });
-  })
+    }
+  )
 );
