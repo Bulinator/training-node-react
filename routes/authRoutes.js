c6c371a09816a63651fc4f52347fecd49bdbcf93
bulotge <bulotge@gisis.eu>
2018-05-17 10:01:31 +0200

    fixed untracked files

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,12 @@ module.exports = app => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  // req = request, incoming, res = outgoing, response
+  app.get('/api/logout', (req, res) => {
+    req.logout(); // attached to express server, erase cookie stored
+    res.send(req.user); // proove user is no longer signed
+  })
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
