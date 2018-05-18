03a86456755aed844c46057d81483354b3dd454e
bulotge <bulotge@gisis.eu>
2018-05-18 16:35:45 +0200

    add payment component with stripe api

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -8,13 +9,12 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return (
-          <li><a href="/auth/google">Sign in with Google</a></li>
-        );
+        return <li><a href="/auth/google">Sign in with Google</a></li>;
       default:
-        return (
-          <li><a href="/api/logout">Logout</a></li>
-        );
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
 
@@ -23,7 +23,7 @@ class Header extends Component {
       <nav>
         <div className="nav-wrapper">
           <Link
-            to={this.props.auth ? '/surveys' : '/' } 
+            to={this.props.auth ? '/surveys' : '/' }
             className="left brand-logo"
           >
             Emaily
