119b83a8ec01e753138d5ae789d77f8470d1a62b
bulotge <bulotge@gisis.eu>
2018-05-18 14:41:53 +0200

    add brand link redirect

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return(
+    <div className="center-align">
+      <h1 className="red-text lighten-2">Emaily tool</h1>
+      Collect feedback from users.
+    </div>
+  );
+};
+
+export default Landing;
