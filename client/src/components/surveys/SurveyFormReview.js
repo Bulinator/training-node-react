eb061bbbbac6b6f579252e9e979a28c2a71ae28f
bulotge <bulotge@gisis.eu>
2018-05-24 15:12:07 +0200

    add survey form frontend

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,45 @@
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import formFields from './formFields';
+import * as actions from '../../actions';
+
+const SurveyReview = ({ onCancel, formValues, submitSurvey, history }) => {
+  const reviewFields = _.map(formFields, ({ label, name }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>
+          {formValues[name]}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h5>Please confirm your entries</h5>
+      {reviewFields}
+      <button
+        className="yellow darken-2 btn-flat white-text"
+        onClick={onCancel}
+      >
+        Back to form
+      </button>
+      <button
+        className="green btn-flat right white-text"
+        onClick={() => submitSurvey(formValues, history)}
+      >
+        Send survey
+        <i className="material-icons right">email</i>
+      </button>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return { formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps, actions)(withRouter(SurveyReview));
---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+  { label: 'Survey title', name: 'title', noValueError: 'You must provide a title' },
+  { label: 'Subject line', name: 'subject', noValueError: 'You must provide a subject' },
+  { label: 'Email body', name: 'body', noValueError: 'You must provide a email body' },
+  { label: 'Recipient list', name: 'recipients', noValueError: 'You must provide one email at least' }
+];
