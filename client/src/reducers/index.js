c6815e70e833e6b14568ac3c31deaad60f230740
bulotge <bulotge@gisis.eu>
2018-05-29 17:07:46 +0200

    debug heroku redux form reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
-import auth from './authReducer';
-import surveys from './surveysReducer';
+import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
-  auth,
+  auth: authReducer,
   form: reduxForm,
-  surveys
+  surveys: surveysReducer
 });
